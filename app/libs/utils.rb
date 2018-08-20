343d8463c8b6c23feda253b85fce35d9c87f40af
Thiago Felipe Peçanha <thiagofelipepecanha@gmail.com>
2018-08-21 00:43:45 +0100

    Creating an util object to hash method

--- app/libs/utils.rb
+++ app/libs/utils.rb
@@ -1,6 +1,14 @@
 # frozen_string_literal: true
 
 module Utils
+  class << self
+    def obj_to_hash(obj)
+      obj.instance_variables.each_with_object({}) do |value, hash|
+        hash[value.to_s.delete('@')] = obj.instance_variable_get(value)
+      end
+    end
+  end
+
   class Twitter
     class << self
       def create_profile_link(args)
