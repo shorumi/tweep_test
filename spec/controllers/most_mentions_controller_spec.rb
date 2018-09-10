81c3c1a56b88b5c82cd8b059c9d9956446e8b1ea
Thiago Felipe Peçanha <thiagofelipepecanha@gmail.com>
2018-09-10 20:33:46 -0300

    Removing unnecessary validations

--- spec/controllers/most_mentions_controller_spec.rb
+++ spec/controllers/most_mentions_controller_spec.rb
@@ -12,23 +12,6 @@ RSpec.describe MostMentionsController, type: :controller do
     end
   end
 
-  context 'when filtering' do
-    let(:records) { response_body['data'].size }
-
-    before(:each) do
-      FactoryBot.create_list(:most_mention, 4)
-      get :index, params: param, format: :json
-    end
-
-    context 'and no filter at all' do
-      let(:param) { {} }
-      it_behaves_like 'a successful response'
-      it 'returns a number of most mention resources correctly' do
-        expect(records).to eq(4)
-      end
-    end
-  end
-
   context 'returns most mentioned tweets as json' do
     describe '#index' do
       before(:each) do
--- spec/controllers/most_relevants_controller_spec.rb
+++ spec/controllers/most_relevants_controller_spec.rb
@@ -12,23 +12,6 @@ RSpec.describe MostRelevantsController, type: :controller do
     end
   end
 
-  context 'when filtering' do
-    let(:records) { response_body['data'].size }
-
-    before do
-      FactoryBot.create_list(:most_relevant, 4)
-      get :index, params: param, format: :json
-    end
-
-    context 'and no filter at all' do
-      let(:param) { {} }
-      it_behaves_like 'a successful response'
-      it 'returns number of variation deviation notes correctly' do
-        expect(records).to eq(4)
-      end
-    end
-  end
-
   context 'returns most relevants tweets as json' do
     describe '#index' do
       before(:each) do
